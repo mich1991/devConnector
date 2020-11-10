5ed05d3897f7c16f0cc306c3df252f0ee17057ea
Michal Wozniak <wozniak.michal91@gmail.com>
2020-11-10 11:46:24 +0000

    Delete experience done

--- routes/api/profiles.js
+++ routes/api/profiles.js
@@ -159,8 +159,8 @@ router.delete('/', authMiddleware, async (req, res) => {
 })
 
 
-// @route   delete api/Profile
-// @desc    delete profile , user & posts
+// @route   PUT api/profile/experience
+// @desc    Add experience to profile
 // @access  Private
 router.put('/experience', [
     authMiddleware,
@@ -214,4 +214,27 @@ router.put('/experience', [
         res.status(500).send('Server Error')
     }
 })
+
+// @route   DELETE api/profile/experience/:exp_id
+// @desc    delete experience from profile
+// @access  Private
+router.delete('/experience/:exp_id', authMiddleware, async (req, res) => {
+    try {
+        const profile = await Profile.findOne({ user: req.user.id });
+
+        // Get remove index
+        const removeIndex = profile.experience.map(item => item.id).indexOf(req.params.exp_id)
+
+        profile.experience.splice(removeIndex, 1)
+
+        await profile.save()
+
+        res.json(profile)
+
+    } catch (error) {
+        console.error(error.message)
+        res.status(500).send('Server Error')
+    }
+})
+
 module.exports = router;
