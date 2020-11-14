bb6a92a4541bf70950313df255895209b82aac97
Michal Wozniak <wozniak.michal91@gmail.com>
2020-11-14 18:31:18 +0000

    Login & Register components finished

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react'
-
 const Register = () => {
 
     const [formData, setFormData] = useState({
@@ -13,12 +12,12 @@ const Register = () => {
 
     const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value })
 
-    const onSubmit = e => {
+    const onSubmit = async e => {
         e.preventDefault()
         if (password !== password2) {
             console.log('Passwords do not match')
         } else {
-            console.log(formData)
+            console.log('Success')
         }
     }
 
