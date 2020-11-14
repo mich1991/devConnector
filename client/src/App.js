5039045fecdcb25e270ae64780800de78b74a3d9
Michal Wozniak <wozniak.michal91@gmail.com>
2020-11-14 16:55:20 +0000

    Form onSubmit & onChange added

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,19 @@ import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
 import Landing from './components/layout/Landing'
+import Login from './components/auth/Login'
+import Register from './components/auth/Register'
 const App = () => (
   <Router>
     <>
       <Navbar />
       <Route exact path='/' component={Landing} />
+      <section className="container">
+        <Switch>
+          <Route exact path='/register' component={Register} />
+          <Route exact path='/login' component={Login} />
+        </Switch>
+      </section>
     </>
   </Router>
 )
