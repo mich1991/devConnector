61531fd092b1e8310f71a72cf3a8f8c956fa480d
Michal Wozniak <wozniak.michal91@gmail.com>
2020-11-18 16:36:05 +0000

    Spinner added, dashboard profile notice

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { REGISTER_FAIL, REGISTER_SUCCESS, USER_LOADED, AUTH_ERROR, LOGIN_FAIL, LOGIN_SUCCESS, LOG_OUT } from './types'
+import { REGISTER_FAIL, REGISTER_SUCCESS, USER_LOADED, AUTH_ERROR, LOGIN_FAIL, LOGIN_SUCCESS, LOG_OUT, CLEAR_PROFILE } from './types'
 import { setAlert } from './alert'
 import setAuthToken from '../utils/setAuthToken'
 
@@ -94,5 +94,8 @@ export const login = (email, password) => async dispatch => {
 export const logout = () => dispatch => {
     dispatch({
         type: LOG_OUT
+    })
+    dispatch({
+        type: CLEAR_PROFILE
     })
 }
--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -5,7 +5,8 @@ import {
     AUTH_ERROR,
     LOGIN_SUCCESS,
     LOGIN_FAIL,
-    LOG_OUT
+    LOG_OUT,
+    CLEAR_PROFILE
 } from '../actions/types'
 
 const initialState = {
@@ -44,6 +45,13 @@ export default function (state = initialState, action) {
                 ...state,
                 token: null,
                 isAuthenticated: false,
+                loading: false
+            }
+        case CLEAR_PROFILE:
+            return {
+                ...state,
+                profile: null,
+                repos: [],
                 loading: false
             }
         default:
