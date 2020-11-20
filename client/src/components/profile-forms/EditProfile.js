82ebb23532849278aaf8f12d45631825cd0a5525
Michal Wozniak <wozniak.michal91@gmail.com>
2020-11-20 17:39:10 +0000

    Editing Profile/ Creating ddone

--- client/src/components/profile-forms/EditProfile.js
+++ client/src/components/profile-forms/EditProfile.js
@@ -1,11 +1,16 @@
-import React, { useState, Fragment } from 'react'
+import React, { useState, Fragment, useEffect } from 'react'
 import { Link, withRouter } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
-import { createProfile } from '../../actions/profile'
+import { createProfile, getCurrentProfile } from '../../actions/profile'
 
 
-const CreateProfile = ({ createProfile, history }) => {
+const EditProfile = ({
+    createProfile,
+    getCurrentProfile,
+    profile: { profile, loading },
+    history
+}) => {
     const [formData, setFormData] = useState({
         company: '',
         website: '',
@@ -22,6 +27,25 @@ const CreateProfile = ({ createProfile, history }) => {
     })
 
     const [displaySocialInputs, toggleSocialInputs] = useState(false)
+
+    useEffect(() => {
+        getCurrentProfile();
+
+        setFormData({
+            company: loading || !profile.company ? '' : profile.company,
+            website: loading || !profile.website ? '' : profile.website,
+            location: loading || !profile.location ? '' : profile.location,
+            status: loading || !profile.status ? '' : profile.status,
+            skills: loading || !profile.skills ? '' : profile.skills + '',
+            githubusername: loading || !profile.githubusername ? '' : profile.githubusername,
+            bio: loading || !profile.bio ? '' : profile.bio,
+            twitter: loading || !profile.social ? '' : profile.social.twitter,
+            facebook: loading || !profile.social ? '' : profile.social.facebook,
+            linkedin: loading || !profile.social ? '' : profile.social.linkedin,
+            youtube: loading || !profile.social ? '' : profile.social.youtube,
+            instagram: loading || !profile.social ? '' : profile.social.instagram,
+        })
+    }, [loading])
 
     const {
         company,
@@ -45,13 +69,13 @@ const CreateProfile = ({ createProfile, history }) => {
 
     const onSubmit = e => {
         e.preventDefault()
-        createProfile(formData, history)
+        createProfile(formData, history, true)
     }
 
     return (
         <Fragment>
             <h1 className="large text-primary">
-                Create Your Profile
+                Edit Your Profile
       </h1>
             <p className="lead">
                 <i className="fas fa-user"></i> Let's get some information to make your
@@ -147,16 +171,19 @@ const CreateProfile = ({ createProfile, history }) => {
 
                 <input type="submit" className="btn btn-primary my-1" />
                 <Link className="btn btn-light my-1" to="/dashboard">Go Back</Link>
-
             </form>
         </Fragment>
     )
 }
 
-CreateProfile.propTypes = {
+EditProfile.propTypes = {
     createProfile: PropTypes.func.isRequired,
+    getCurrentProfile: PropTypes.func.isRequired,
+    profile: PropTypes.object.isRequired,
 }
 
+const mapStateToProps = state => ({
+    profile: state.profile
+})
 
-
-export default connect(null, { createProfile })(withRouter(CreateProfile))
+export default connect(mapStateToProps, { createProfile, getCurrentProfile })(withRouter(EditProfile))
